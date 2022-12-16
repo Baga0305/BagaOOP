c6f07116f4067c7d89c02407642e907dfdb76e06
Baga <bagahalimbekov00@gmail.com>
2022-12-16 20:29:25 +0300

    Перегрузка методов и конструкторов

--- app/src/main/java/com/example/myapplication/Cat.java
+++ app/src/main/java/com/example/myapplication/Cat.java
@@ -2,8 +2,10 @@ package com.example.myapplication;
 
 import android.util.Log;
 
+import android.util.Log;
+
 public class Cat extends  Animal {
-    protected int age;
+    int age;
     String name;
     String breed;
     String color;
@@ -22,5 +24,11 @@ public class Cat extends  Animal {
 
     public void talk(){
         Log.i("talk()", "Meow! My name is " + name + ", and I'm " + age + " yaers old. My breed " + breed + ", my color is " + color);
+    }
+    public void talk(int age){
+        Log.i("talk()", "Meow! I'm " + age + " yaers old.");
+    }
+    public void talk(String hello){
+        Log.i("talk()", "Meow! " + hello);
     }
 }
